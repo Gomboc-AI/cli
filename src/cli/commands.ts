d30a1805d70b9ada7db01a441c061fe30703a1b4
Pepe GC <jpgarcia90@gmail.com>
2024-02-02 14:29:33 -0500

    Dev 335 support multiple directories (#1) (#46)

--- src/cli/commands.ts
+++ src/cli/commands.ts
@@ -1,23 +1,23 @@
 export enum VerbCommand {
-    REMEDIATE = 'remediate'
+  REMEDIATE = 'remediate'
 }
 
 export enum SourceCommand {
-    LOCAL = 'local',
-    REMOTE = 'remote'
+  LOCAL = 'local',
+  REMOTE = 'remote'
 }
 
 export enum ServiceCommand {
-    CLOUDFORMATION = 'cloudformation',
-    TERRAFORM = 'terraform'
+  CLOUDFORMATION = 'cloudformation',
+  TERRAFORM = 'terraform'
 }
 
 export enum ClientCommand {
-    GITHUB = 'github',
-    GITLAB = 'gitlab'
+  GITHUB = 'github',
+  GITLAB = 'gitlab'
 }
 
 export enum EffectCommand {
-    DIRECT_APPLY = 'direct-apply',
-    SUBMIT_FOR_REVIEW = 'submit-for-review'
+  SUBMIT_FOR_REVIEW = 'submit-for-review',
+  DIRECT_APPLY = 'direct-apply'
 }
--- src/cli/interfaces/remediate.ts
+++ src/cli/interfaces/remediate.ts
@@ -2,11 +2,12 @@ import { Arguments } from "yargs"
 
 import { resolve as resolveRemediateRemoteTfHCL2, Inputs as RemediateRemoteTfHCL2Inputs } from "../../resolvers/remediateRemoteTfHCL2.js"
 import { ExitCode } from "../exitCodes.js"
-import { EffectCommand } from "../commands.js"
 import { ConsoleLogger } from "../../utils/ConsoleLogger.js"
 import { settings } from "../../settings.js"
 import { consoleDebugger } from "../../utils/ConsoleDebugger.js"
 import { Effect } from "../../apiclient/gql/graphql.js"
+import { hl } from "../../utils/consoleUtils.js"
+import { EffectCommand } from "../commands.js"
 
 
 export const cliTerraformRemediateRemoteCheck = async (argv: Arguments): Promise<ExitCode> => {
@@ -16,13 +17,55 @@ export const cliTerraformRemediateRemoteCheck = async (argv: Arguments): Promise
     // argv._[2] -> SourceCommand (remote, local)
     // argv._[3] -> EffectCommand (submit-for-review, direct-apply)
 
+    const cl = new ConsoleLogger(false)
+
+    const workingDirectoryOption = argv.workingDirectory ? argv.workingDirectory as string : null
+    const targetDirectoriesOption = argv.targetDirectotries ? argv.targetDirectories as string[] : null
+
+    const getDesiredEffect = (effect: string): Effect => {
+      switch (effect as EffectCommand) {
+        case EffectCommand.SUBMIT_FOR_REVIEW:
+          return Effect.SubmitForReview
+        case EffectCommand.DIRECT_APPLY:
+          return Effect.DirectApply
+        default:
+          throw new Error(`Invalid effect: ${effect}.`)
+      }
+    }
+
+    /**
+     * We want to make sure that only one of these two options is used
+     * @param workingDirectoryOption string -- is the old option that will be removed in the future
+     * @param targetDirectoriesOption string[] -- is the new option
+     */
+    const getTargetDirectories = (workingDirectoryOption: string | null, targetDirectoriesOption: string[] | null): string[] => {
+      const bothOptionsArePresent = workingDirectoryOption != null && targetDirectoriesOption != null
+      const bothOptionsAreMissing = workingDirectoryOption == null && targetDirectoriesOption == null
+      const onlyOldOptionPresent = workingDirectoryOption != null && targetDirectoriesOption == null
+
+      if (bothOptionsArePresent) {
+        const msg = 'Cannot use both working-directory and target-directories options. Please use target-directories only'
+        cl.err(ExitCode.INVALID_ARGUMENTS, msg)
+      } else if (bothOptionsAreMissing) {
+        const msg = 'Please specify a least one target directory'
+        cl.err(ExitCode.INVALID_ARGUMENTS, msg)
+      } else if (onlyOldOptionPresent) {
+        cl.log(hl('DEPRECATION NOTICE: working-directory is deprecated and will be removed in the future. Please use target-directories instead'))
+        return [workingDirectoryOption]
+      }
+      // One case left: only the new option was passed
+      return targetDirectoriesOption as string[] // safe to coerce, TS cannot infer
+    }
+
+    if (argv.accessToken){
+      cl.log(hl('DEPRECATION NOTICE: access-token is deprecated and will be removed in the future'))
+    }
+
     const inputs: RemediateRemoteTfHCL2Inputs = {
       authToken: argv.authToken as string,
-      output: argv.output as string,
       serverUrl: settings.SERVER_URL,
-      workingDirectory: argv.workingDirectory as string,
-      effect: argv._[3]== EffectCommand.SUBMIT_FOR_REVIEW ? Effect.SubmitForReview : Effect.DirectApply,
-      accessToken: argv.accessToken as string
+      targetDirectories: getTargetDirectories(workingDirectoryOption, targetDirectoriesOption),
+      effect: getDesiredEffect(argv._[3] as string),
     }
 
     consoleDebugger.log('CLI inputs', inputs)
