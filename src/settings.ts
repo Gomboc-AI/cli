b8935cfde0976467f96b7af08ac85afd23cb07c9
justinkim <jkim@gomboc.ai>
2025-04-11 14:23:57 -0400

    resolve empty string case

--- src/settings.ts
+++ src/settings.ts
@@ -6,12 +6,30 @@ export type Settings = {
   DEBUG_MODE: boolean;
 }
 
+const DEFAULT_GOMBOC_SERVER_URL = 'https://scan.app.gomboc.ai/graphql'
+const DEFAULT_GOMBOC_CLIENT_URL = 'https://app.gomboc.ai'
+
 export const getSettings = (): Settings => {
-  const gombocServerUrl = process.env.GOMBOC_SERVER_URL
-  const gombocClientUrl = process.env.GOMBOC_CLIENT_URL
+  let gombocServerUrl = process.env.GOMBOC_SERVER_URL ?? ''
+  let gombocClientUrl = process.env.GOMBOC_CLIENT_URL ?? ''
+
+  try {
+    new URL(gombocClientUrl)
+  } catch (e) {
+    gombocClientUrl = DEFAULT_GOMBOC_CLIENT_URL
+  }
+  try {
+    new URL(gombocServerUrl)
+  } catch (e) {
+    gombocServerUrl = DEFAULT_GOMBOC_SERVER_URL
+  }
+
+  console.log(`Connecting to client at ${gombocClientUrl}`)
+  console.log(`Connecting to server at ${gombocServerUrl}`)
+
   const settings = {
-    SERVER_URL: gombocServerUrl == null || gombocServerUrl === '' ? 'https://scan.app.gomboc.ai/graphql' : gombocServerUrl,
-    CLIENT_URL: gombocClientUrl == null || gombocClientUrl === '' ? 'https://app.gomboc.ai' : gombocClientUrl,
+    SERVER_URL: gombocServerUrl,
+    CLIENT_URL: gombocClientUrl,
     DEBUG_MODE: process.env.GOMBOC_DEBUG != null,
   }
   return settings
