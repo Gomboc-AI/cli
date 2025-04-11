3b17e9807cebae8bf0f731257619234a4a19e078
Justin Kim <39386551+JustinKim3420@users.noreply.github.com>
2025-04-11 15:00:48 -0400

    Dev-2315 cli should only fail block action if at least one auto remediated (#93)
    * updated query
    
    * updated version
    
    * additional logs
    
    * removed circular dependency
    
    * resolve empty string case
    
    * resolve empty string case

--- src/settings.ts
+++ src/settings.ts
@@ -1,13 +1,39 @@
+
+
 export type Settings = {
   SERVER_URL: string;
   CLIENT_URL: string;
   DEBUG_MODE: boolean;
 }
 
-export const getSettings = (): Settings => ({
-  SERVER_URL: process.env.GOMBOC_SERVER_URL ?? 'https://scan.app.gomboc.ai/graphql',
-  CLIENT_URL: process.env.GOMBOC_CLIENT_URL ?? 'https://app.gomboc.ai',
-  DEBUG_MODE: process.env.GOMBOC_DEBUG != null,
-})
+const DEFAULT_GOMBOC_SERVER_URL = 'https://scan.app.gomboc.ai/graphql'
+const DEFAULT_GOMBOC_CLIENT_URL = 'https://app.gomboc.ai'
+
+export const getSettings = (): Settings => {
+  let gombocServerUrl = process.env.GOMBOC_SERVER_URL ?? ''
+  let gombocClientUrl = process.env.GOMBOC_CLIENT_URL ?? ''
+
+  try {
+    new URL(gombocClientUrl)
+  } catch (e) {
+    gombocClientUrl = DEFAULT_GOMBOC_CLIENT_URL
+  }
+  try {
+    new URL(gombocServerUrl)
+  } catch (e) {
+    gombocServerUrl = DEFAULT_GOMBOC_SERVER_URL
+  }
+
+  console.log(`Connecting to client at ${gombocClientUrl}`)
+  console.log(`Connecting to server at ${gombocServerUrl}`)
+
+  const settings = {
+    SERVER_URL: gombocServerUrl,
+    CLIENT_URL: gombocClientUrl,
+    DEBUG_MODE: process.env.GOMBOC_DEBUG != null,
+  }
+  return settings
+}
+
 
 export const settings = getSettings();
